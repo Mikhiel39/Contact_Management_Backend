df3589c73277cb5158270e70776251664cf04049
Mikhiel <mikhielbenji9@gmail.com>
2024-11-17 14:10:25 +0530

    Backend code

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/contact_management', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost:27017/contact_management", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
